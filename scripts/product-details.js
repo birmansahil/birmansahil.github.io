992f87682d4e93dff8c4d4a369ee367e269fdb19
birmansahil <contact.birmansahil@gmail.com>
2024-08-08 15:43:59 -0400

    added description, features, and dimensions in json object. Added product details page

--- scripts/product-details.js
+++ scripts/product-details.js
@@ -1,10 +1,34 @@
-let queryString = window.location.search;
-let urlParams = new URLSearchParams(queryString);
+const productDetails = document.querySelector(".product-details");
+
+const urlParams = new URLSearchParams(window.location.search);
+const productId = parseInt(urlParams.get('product-id'));
 
 fetch("products.json").then((response) => {
     return response.json();
 }).then(data => {
-    return data.find(product => product.id == urlParams.get("product-id"))
-}).then(product => {
-    console.log(product);
-});
+    const product = data.find(product => product.id == productId)
+    
+    productDetails.innerHTML += `
+    <img src=${product.imageSource} alt=${product.imageAlt} />
+
+    <div class="product-meta">
+        <h2>${product.name} by ${product.student}</h2>
+        <h3>${product.price}</h3>
+
+        <h4>Description</h4>
+        <p>${product.description}</p>
+
+        <h4>Features<h4>
+        <ul>
+            <li>${product.features[0]}</li>
+            <li>${product.features[1]}</li>
+            <li>${product.features[2]}</li>
+        </ul>
+
+        <h4>Dimensions</h4>
+        <p>${product.dimensions}</p>
+
+        <a class="btn" href="#">Add to cart</a>
+    </div>
+    `
+})
