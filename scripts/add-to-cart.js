5c3a5b642fcdda5d4fd22cb33bb10a70ca84d114
birmansahil <contact.birmansahil@gmail.com>
2024-08-15 11:39:59 -0400

    added notification for item to cart

--- scripts/add-to-cart.js
+++ scripts/add-to-cart.js
@@ -1,12 +1,26 @@
+const notification = document.querySelector(".notification");
+
 function addToCart(productId) {
     let cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
     if (cart.find((item) => item == productId)) {
-        console.log("item already in cart");
+        notification.innerHTML = `<p>Item already in cart</p>`;
+        showNotification();
+        setTimeout(hideNotification, 2000)
     } else {
         cart.push(productId);
-        console.log("item added to cart");
+        notification.innerHTML = `<p>Item added to cart</p>`;
+        showNotification();
+        setTimeout(hideNotification, 2000)
     }
     
     localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+
+function showNotification() {
+    notification.classList.add("show");
+}
+
+function hideNotification() {
+    notification.classList.remove("show");
 }
