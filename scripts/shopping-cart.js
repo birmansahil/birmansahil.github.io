2aac8fd1686f4bda03a711e7f0fae5656e807455
birmansahil <contact.birmansahil@gmail.com>
2024-08-15 11:19:19 -0400

    added shopping cart

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,3 +1,31 @@
-let cart = localStorage.getItem("shoppingCart");
+const cartProducts = document.querySelector(".cart-products");
 
-cart.map()
+let cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+
+function renderCart() {
+    fetch("products.json").then((response) => {
+        return response.json();
+    }).then(data => {
+        const products = data.filter(product => cart.includes(product.id));
+    
+        products.map(product => {
+            cartProducts.innerHTML += `
+            <tr>
+                <td><img src=${product.imageSource} alt=${product.imageAlt} /> ${product.name}</td>
+                <td><button class="remove-item-btn" onclick="removeItem(${product.id})">Remove Item</button></td>
+                <td>${product.price}</td>
+            </tr>
+            `
+        })
+    })
+}
+
+function removeItem(productId) {
+    console.log("you have requested to remove", productId);
+    cart = cart.filter(itemId => itemId !== productId);
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
+    cartProducts.innerHTML = ``
+    renderCart();
+}
+
+renderCart();
