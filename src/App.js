e33891ee2f9ab93dda217fdbde0ccefe750b0223
birmansahil <contact.birmansahil@gmail.com>
2024-12-17 19:21:26 -0500

    created landing page

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Intro from './intro';
+import './App.css';
+
+function App() {
+  return (
+    <Intro />
+  );
+}
+
+export default App;
---
+++ src/intro.js
@@ -0,0 +1,20 @@
+import React from "react";
+import birmansahil from "./birmansahil.webp";
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <h1>creatvie designer<br />& developer.</h1>
+            <p>Hi I am Sahil Birman. A passionate Designer and Front End Developer based in North York, Canada.</p>
+            <a href="#">See my work &rarr;</a>
+
+            <div>
+                <a href="#">LinkedIn</a> / 
+                <a href="#">GitHub</a> / 
+                <a href="#">Dribble</a>
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
