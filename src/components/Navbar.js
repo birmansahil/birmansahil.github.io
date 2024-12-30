1f433c761c7bde13a1cf53e2108693aa6de6e894
birmansahil <contact.birmansahil@gmail.com>
2024-12-29 20:16:26 -0500

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,10 @@ import './Navbar.css';
 
 const Navbar = () => {
     return(
+
         <nav className="navbar">
             <ul className="nav-list">
-                <li><a className="nav-list-link">Work</a></li>
+                <li><a className="nav-list-link active">Work</a></li>
                 <li><a className="nav-list-link">About</a></li>
                 <li><a className="nav-list-link">Contact</a></li>
                 <li><a className="nav-list-link">Work</a></li>
